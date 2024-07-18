0f351cfdb2392d053a8dea0892e705180196ea04
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-07-18 23:22:42 +0530

    chore: Update ShareDashboardModal to filter users based on permission level

--- src/components/ShareDashboardModal.tsx
+++ src/components/ShareDashboardModal.tsx
@@ -115,6 +115,7 @@ const ShareDashboardModal = () => {
       return selectedUser;
     });
     setSelectedUsers(updatedUsers);
+    setFilteredUsers(updatedUsers);
   };
 
   const handleFilterChange = (filter: string) => {
